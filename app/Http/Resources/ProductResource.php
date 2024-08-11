d41107e01634145930eefb6b4dbeb69e962a02ac
Saleh Goied <salehgoied@gmail.com>
2024-08-11 13:43:39 +0300

    remove user id from products Seeder

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -16,7 +16,6 @@ class ProductResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'user_id' => $this->user_id,
             'name' => $this->name,
             'description' => $this->description,
             'price' => $this->price,
